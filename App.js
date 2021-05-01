5574e8527c6d20e760e8f2677672f66db6db3e59
chrockny <chrockny1007@gmail.com>
2021-04-30 20:06:01 -0500

    Esconder teclado con Keyboard y TouchableWithoutFeedback

--- App.js
+++ App.js
@@ -1,127 +1,63 @@
 import React, {useState} from 'react';
-import { StyleSheet,ScrollView,FlatList,Platform,TouchableOpacity, Text, View,Button ,TextInput} from 'react-native';
-
+import { StyleSheet,TouchableWithoutFeedback,Keyboard,Alert,ScrollView,SafeAreaView,FlatList,Platform,TouchableOpacity, Text, View,Button ,TextInput} from 'react-native';
+import Header from './components/Header';
+import Task from './components/Task';
+import AddTask from './components/AddTask';
+import {v4 as uuidv4} from 'uuid'; 
 export default  function App(){
+  const [tasks,setTasks] = useState([
+      {"task":"HTML I","done":true,"id":"1"},
+      {"task":"CSS","done":true,"id":"2"},
+      {"task":"Responsive design","done":true,"id":"3"},
+  ]);
 
-  const [todos, setTodos] = useState([{
-    "userId": 1,
-    "id": 1,
-    "title": "delectus aut autem",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 2,
-    "title": "quis ut nam facilis et officia qui",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 3,
-    "title": "fugiat veniam minus",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 4,
-    "title": "et porro tempora",
-    "completed": true
-  },
-  {
-    "userId": 1,
-    "id": 5,
-    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 6,
-    "title": "qui ullam ratione quibusdam voluptatem quia omnis",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 7,
-    "title": "illo expedita consequatur quia in",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 8,
-    "title": "quo adipisci enim quam ut ab",
-    "completed": true
-  },
-  {
-    "userId": 1,
-    "id": 9,
-    "title": "molestiae perspiciatis ipsa",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 10,
-    "title": "illo est ratione doloremque quia maiores aut",
-    "completed": true
-  }])
-
-
-  const pressHandler = (id)=>{
-    //Creando un nuevo Array llamador returnValue
-    let returnValue = [...todos];
-    returnValue.forEach(todo=>{
-      //Verificando que de todos los id's de 'todos'
-      //El que coincida con el id del evento onPress cambie su estado
-      if(todo.id === id){
-        todo.completed = true
-      }
-    })
-    setTodos(returnValue)
+  const addTask = (text) =>{
+    if(!text){
+      Alert.alert('No tasks?',
+      'Please add a task',[{
+        text:'OK'
+      }])
+    }
+    else{
+    setTasks(prevTasks=>{
+      return [{task:text,id:uuidv4()}
+      ,...prevTasks]
+    })}
   }
 
-
   return (
-    <View style={styles.container}>
-      <FlatList
-      numColumns={3}
-      data={todos}
-      keyExtractor={(item)=>item.id.toString()}
-      renderItem={({item}) => (
-
-      <TouchableOpacity
-      onPress={()=>pressHandler(item.id)}>
-<Text style={{...styles.todo,
-backgroundColor:item.completed?'green':
-'deeppink'}}>{item.title}</Text>
-      </TouchableOpacity>
-      )}/>
+    <TouchableWithoutFeedback 
+    onPress={()=>
+/* Esconder teclado al darle click en cualquier parte de mi app*/
+    Keyboard.dismiss()}>
+    <SafeAreaView style={styles.container}>
+      <Header />
+      <View style={styles.content}>
+        <AddTask addTask={addTask}/>
+        <View style={styles.list}>
+        <FlatList
+        data={tasks}
+        renderItem={({item})=>(
+          <Task item={item}/>
+                )}/>
+        </View>
       </View>
-  /*<ScrollView  style={styles.container}>
-
-      {todos.map(todo=>{
-        return(
-      <View
-      key={todo.id}
-      style={styles.todo}>
-        <Text
-        >{todo.title}</Text>
-      </View>)
-      })}
-      </ScrollView>
-    */);
+    </SafeAreaView>
+    </TouchableWithoutFeedback>
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    //alignItems: 'center',
-    //justifyContent: 'center',
+
   },
-  todo:{
-    margin:15,
-    color:'white',
-    fontSize:15,
-    padding:15,
+  content:{
+    padding:30,
+  },
+  list:{
+    marginTop:30,
   },
 });
 
